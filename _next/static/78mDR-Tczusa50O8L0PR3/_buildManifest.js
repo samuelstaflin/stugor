a2b094c0ad816950df93f5a1d9e097ca84170adc
zaamme <57627333+samuelstaflin@users.noreply.github.com>
2022-06-01 14:34:01 +0200

    fungerande tailwind

--- _next/static/78mDR-Tczusa50O8L0PR3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1029025ee9e7222e.js"],"/404":["static/chunks/pages/404-a5f13c79a98167e6.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
