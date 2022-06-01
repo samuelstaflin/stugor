59bb1afd30a1bb2d277b56b26ec3ac9ed1de1c67
zaamme <57627333+samuelstaflin@users.noreply.github.com>
2022-06-01 19:54:48 +0200

    lite mer hemsida

---
+++ _next/static/JJno4N9nKBWjpySYZrBWM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/252f366e-ce9bed3bbe1b6493.js","static/chunks/1bfc9850-3d19129a0bc085ba.js","static/chunks/pages/index-1e7204f93db3fa07.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
